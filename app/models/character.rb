1a9c0c2d346d1c1a64f28215c7a2492ca1c5c24f
JazzyMussels <jeffmussels@gmail.com>
2022-01-14 05:52:46 -0500

    updated nv vars

--- app/models/character.rb
+++ app/models/character.rb
@@ -33,8 +33,14 @@ class Character < ApplicationRecord
             
           }
 
+          credly_headers = {
+            :Accept => "application/json",
+            :Authorization => "Basic #{ENV['CREDLY_AUTH'][0...-1]}",
+            "Content-Type" => "application/json"
+        }
+
  
-          RestClient::Request.execute(method: :post, url: "https://sandbox-api.credly.com/v1/organizations/#{$org_id}/badges", payload: response_body, headers: $credly_headers)
+          RestClient::Request.execute(method: :post, url: "https://sandbox-api.credly.com/v1/organizations/#{ENV['CREDLY_ORG_ID']}/badges", payload: response_body, headers: credly_headers)
     
           CharacterBadge.create(badge_id: badge_id, character_id: character_id, recipient_email: email, issued_to_first_name: first_name,  issued_to_last_name: last_name, badge_template_id: badge_template_id, issued_at: issued_at )
 
